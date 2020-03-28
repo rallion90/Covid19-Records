514a72f66b808d751ba23051b220cd0b52ad48c2
unknown <ralphlaynes@gmail.com>
2020-03-28 20:40:57 +0800

    Newest

--- asset/main.js
+++ asset/main.js
@@ -13,12 +13,14 @@ $.ajax({
             var recovered = data.recovered.toLocaleString();
             var today_deaths = data.todayDeaths.toLocaleString();
             var today_cases = data.todayCases.toLocaleString();
+            var logo = data.countryInfo.flag;
             $('#cases').html(confirmed);
             $('#active').html(active);
             $('#deaths').html(deaths);
             $('#recovered').html(recovered);
             $('#today_deaths').html(today_deaths);
             $('#today_cases').html(today_cases);
+            $('#logo').attr('href', logo)
 	}
 });
    
